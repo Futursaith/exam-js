eee87b7ca2be5b391b88f070a2864d8466a891bd
Futursaith <saith_7@hotmail.fr>
2021-03-12 17:16:39 +0100

    fiveWay

---
+++ fiveWayLoop.js
@@ -0,0 +1,16 @@
+let tab = ['Julie', 'Djo', 'Raph']
+
+let i = 0
+while (i < tab.length) {
+  console.log(`${tab[i]}${tab[i].length}`)
+  i++
+}
+let j = 0;
+do {
+  console.log(`${tab[j]}${tab[j].length}`);
+  j++;
+} while (j < tab.length);
+
+
+
+
